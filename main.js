9400ce553cb81973620fdda70422a0fe854ff6be
chanki <chanki.kim89@gmail.com>
2023-05-28 14:05:02 +0900

    obj변경

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three'
-import * as TWEEN from '/node_modules/three/examples/jsm/libs/tween.module.js'
-import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MeshSurfaceSampler } from '/node_modules/three/examples/jsm/math/MeshSurfaceSampler.js'
+import * as THREE from "three";
+import * as TWEEN from "/node_modules/three/examples/jsm/libs/tween.module.js";
+import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MeshSurfaceSampler } from "/node_modules/three/examples/jsm/math/MeshSurfaceSampler.js";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -10,78 +10,78 @@ gsap.registerPlugin(ScrollTrigger);
 
 /////////////////////////////////////////////////////////////////////////
 //// DRACO LOADER TO LOAD DRACO COMPRESSED MODELS FROM BLENDER
-const loader = new OBJLoader()
+const loader = new OBJLoader();
 
 /////////////////////////////////////////////////////////////////////////
 ///// DIV CONTAINER CREATION TO HOLD THREEJS EXPERIENCE
-const container = document.createElement('div')
-document.body.appendChild(container)
+const container = document.createElement("div");
+document.body.appendChild(container);
 
 /////////////////////////////////////////////////////////////////////////
 ///// SCENE CREATION
-const scene = new THREE.Scene()
-scene.background = new THREE.Color(0xE39469)
-
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xe39469);
 
 /////////////////////////////////////////////////////////////////////////
 ///// RENDERER CONFIG
-const renderer = new THREE.WebGLRenderer() // turn on antialias
-renderer.setSize(window.innerWidth, window.innerHeight) // make it full screen
-container.appendChild(renderer.domElement) // add the renderer to html div
+const renderer = new THREE.WebGLRenderer(); // turn on antialias
+renderer.setSize(window.innerWidth, window.innerHeight); // make it full screen
+container.appendChild(renderer.domElement); // add the renderer to html div
 
 /////////////////////////////////////////////////////////////////////////
 ///// CAMERAS CONFIG
-const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100)
-camera.position.set(-9,7,-1)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100);
+camera.position.set(0, -2, 10);
+//camera.lookAt(points);
+scene.add(camera);
 
 /////////////////////////////////////////////////////////////////////////
 ///// MAKE EXPERIENCE FULL SCREEN
-window.addEventListener('resize', () => {
-    const width = window.innerWidth
-    const height = window.innerHeight
-    camera.aspect = width / height
-    camera.updateProjectionMatrix()
+window.addEventListener("resize", () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
 
-    renderer.setSize(width, height)
-    composer.setSize(width, height)
-    renderer.setPixelRatio(1)
-    m.uniforms.iResolution.value.set(width, height)
-})
+    renderer.setSize(width, height);
+    composer.setSize(width, height);
+    renderer.setPixelRatio(1);
+    m.uniforms.iResolution.value.set(width, height);
+});
 
 /////////////////////////////////////////////////////////////////////////
 ///// CREATE ORBIT CONTROLS
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enabled =false;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enabled = false;
 /////////////////////////////////////////////////////////////////////////
 ///// LOADING GLB/GLTF MODEL FROM BLENDER
-loader.load('/teamugobj.obj', function (obj) {
-    sampler1 = new MeshSurfaceSampler(obj.children[0]).build()
-    transformMesh1()
+loader.load("/uxis.obj", function (obj) {
+    sampler1 = new MeshSurfaceSampler(obj.children[0]).build();
+    transformMesh1();
     playScrollAnimations();
-})
+});
 
-loader.load('/sphere.obj', function (obj) {
-    sampler2 = new MeshSurfaceSampler(obj.children[0]).build()
-    transformMesh2()
-})
+loader.load("/sphere.obj", function (obj) {
+    sampler2 = new MeshSurfaceSampler(obj.children[0]).build();
+    transformMesh2();
+});
 
 /////////////////////////////////////////////////////////////////////////
 ///// TRANSFORM MESH INTO POINTS
-let sampler1
-let points
-let pointsGeometry = new THREE.BufferGeometry()
-const cursor = {x:0, y:0}
-const vertices1 = []
-const tempPosition1 = new THREE.Vector3()
+let sampler1;
+let points;
+let pointsGeometry = new THREE.BufferGeometry();
+const cursor = { x: 0, y: 0 };
+const vertices1 = [];
+const tempPosition1 = new THREE.Vector3();
 
-function transformMesh1(){
-    for (let i = 0; i < 30000; i ++) {
-        sampler1.sample(tempPosition1)
-        vertices1.push(tempPosition1.x, tempPosition1.y, tempPosition1.z)
+function transformMesh1() {
+    for (let i = 0; i < 30000; i++) {
+        sampler1.sample(tempPosition1);
+        vertices1.push(tempPosition1.x, tempPosition1.y, tempPosition1.z);
     }
-    
-    pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices1, 3))
+
+    pointsGeometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices1, 3));
 
     const pointsMaterial = new THREE.PointsMaterial({
         color: 0x5c0b17,
@@ -90,21 +90,20 @@ function transformMesh1(){
         transparent: true,
         opacity: 0.8,
         depthWrite: false,
-    })
+    });
 
-    points = new THREE.Points(pointsGeometry, pointsMaterial)
+    points = new THREE.Points(pointsGeometry, pointsMaterial);
 
-    scene.add(points)
-
+    scene.add(points);
 }
 
-let sampler2
-const vertices2 = []
+let sampler2;
+const vertices2 = [];
 const tempPosition2 = new THREE.Vector3();
-function transformMesh2(){
-    for (let i = 0; i < 30000; i ++) {
-        sampler2.sample(tempPosition2)
-        vertices2.push(tempPosition2.x, tempPosition2.y, tempPosition2.z)
+function transformMesh2() {
+    for (let i = 0; i < 30000; i++) {
+        sampler2.sample(tempPosition2);
+        vertices2.push(tempPosition2.x, tempPosition2.y, tempPosition2.z);
     }
 }
 
@@ -112,37 +111,34 @@ function transformMesh2(){
 //// INTRO CAMERA ANIMATION USING TWEEN
 function introAnimation() {
     gsap.to(pointsGeometry.attributes.position.array, {
-        endArray : vertices2,
-        duration :2,
-        onUpdate: () => pointsGeometry.attributes.position.needsUpdate = true,
-    })
-    
+        endArray: vertices2,
+        duration: 2,
+        onUpdate: () => (pointsGeometry.attributes.position.needsUpdate = true),
+    });
 }
-
 
 /////////////////////////////////////////////////////////////////////////
 ///// POST PROCESSING EFFECTS
-let width = window.innerWidth
-let height = window.innerHeight
-
+let width = window.innerWidth;
+let height = window.innerHeight;
 
 /////////////////////////////////////////////////////////////////////////
 //// RENDER LOOP FUNCTION
-const animationScripts = []
+const animationScripts = [];
 function rendeLoop() {
-    renderer.render(scene, camera)
-    requestAnimationFrame(rendeLoop) //loop the render function    
+    renderer.render(scene, camera);
+    requestAnimationFrame(rendeLoop); //loop the render function
 }
 
-rendeLoop() //start rendering
+rendeLoop(); //start rendering
 
-let scrollPercent = 0
+let scrollPercent = 0;
 function lerp(x, y, a) {
-    return (1 - a) * x + a * y
+    return (1 - a) * x + a * y;
 }
 
 function scalePercent(start, end) {
-    return (scrollPercent - start) / (end - start)
+    return (scrollPercent - start) / (end - start);
 }
 
 function playScrollAnimations() {
@@ -152,30 +148,29 @@ function playScrollAnimations() {
     //     }
     // })
     gsap.to(pointsGeometry.attributes.position.array, {
-        endArray : vertices2,
+        endArray: vertices2,
         scrollTrigger: {
-            trigger:'.trigger',
-            start:'top top',
-            scrub:1,
+            trigger: ".trigger",
+            start: "top top",
+            scrub: 1,
         },
-        onUpdate: () => pointsGeometry.attributes.position.needsUpdate = true,
-    })
+        onUpdate: () => (pointsGeometry.attributes.position.needsUpdate = true),
+    });
 }
-
 
 animationScripts.push({
     start: 40,
     end: 80,
     func: () => {
-       // if(!pointsGeometry.attributes.position) return;
-        const origin = pointsGeometry.attributes.position.array
-        const _targetArray = vertices2.map((value,idx) => {
-            return lerp(origin[idx], value, scalePercent(40, 80))
-        })
-        pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(_targetArray, 3))
+        // if(!pointsGeometry.attributes.position) return;
+        const origin = pointsGeometry.attributes.position.array;
+        const _targetArray = vertices2.map((value, idx) => {
+            return lerp(origin[idx], value, scalePercent(40, 80));
+        });
+        pointsGeometry.setAttribute("position", new THREE.Float32BufferAttribute(_targetArray, 3));
         //pointsGeometry.attributes.position.needsUpdate = true
     },
-})
+});
 
 // document.body.onscroll = () => {
 //     scrollPercent =
